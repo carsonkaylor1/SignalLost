98d354220bd7fba8f8995c0adf78b80775bc034b
Andrew Barclay <uscdrew1@gmail.com>
2018-10-12 10:45:07 -0400

    Update Signal Lost/Assets/Scripts/pickup.cs

---
+++ Signal Lost/Assets/Scripts/pickup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();	
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up")) //create a tag (under the name of an object) and set all pickups to Pick Up
+        {
+            other.gameObject.SetActive(false);
+            //Depending on which pickup was picked up, do something.
+        }
+    }
+}
