73258b990c4a528fc703070c3b00dc7851d13c83
carsonkaylor1 <ckaylor@email.sc.edu>
2018-10-25 18:23:54 -0400

    AddEnvironementandScripts

---
+++ Signal Lost/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    //Most of this is reused from the in class example
+
+    float velX, velY;
+    float X, Y;
+
+    public float maxVel;
+
+    public float mouseSpeed = 3;
+
+    Rigidbody rigid;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        X = Input.GetAxis("Mouse X") * mouseSpeed;
+        Y = Input.GetAxis("Mouse Y") * mouseSpeed;
+
+        transform.Rotate(-Y, 0, 0);
+
+        velX = Input.GetAxis("Horizontal");
+        velY = Input.GetAxis("Vertical");
+
+    }
+}
---
+++ Signal Lost/Assets/Scripts/LevelControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour {
+
+    public int index;
+    public string levelName;
+
+	// Use this for initialization
+	void OnTriggerEnter (Collider other) {
+        if (other.CompareTag("Player"))
+        {
+           //SceneManager.LoadScene(1);
+
+           SceneManager.LoadScene(levelName);
+        }
+		
+	}
+	
+}
