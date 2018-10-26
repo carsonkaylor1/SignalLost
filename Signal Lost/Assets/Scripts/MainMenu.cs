8e44cdbb3cdc242d5669dd0163710004405b9717
TristanK <frostflag@gmail.com>
2018-10-26 02:55:24 -0400

    Added non-functional armor and health bars. Added functional main menu. Deleted some unused third party assets

---
+++ Signal Lost/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame()
+	{
+		//****NOTE: This only works if the first game scene is 1 higher than this menu in the build index
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("QUIT");
+		Application.Quit();
+	}
+}
