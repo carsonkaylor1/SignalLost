75369db2a3d39673cc9ccc6783e517cada67f813
Andrew Barclay <uscdrew1@gmail.com>
2018-11-01 19:36:43 -0400

    Update Signal Lost/Assets/Scripts/pickup.cs
    Updated Pickup script to allow for distinction between damage types

--- Signal Lost/Assets/Scripts/pickup.cs
+++ Signal Lost/Assets/Scripts/pickup.cs
@@ -2,24 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pickup : MonoBehaviour {
+public class pickup : MonoBehaviour
+{
 
     private Rigidbody rb;
 
     public int health = 5;
     public int armor = 0;
-	public int lastDamageFrame = 0;
+    public int lastDamageFrame = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rb = GetComponent<Rigidbody>();
-	}
+    }
 
     private void OnTriggerEnter(Collider other)
     {
+        string objectName = other.name;
         if (other.gameObject.CompareTag("Pick Up")) //create a tag (under the name of an object) and set all pickups to Pick Up
         {
-            string objectName = other.name;
             switch (objectName)
             {
                 case "healthUp":
@@ -36,33 +38,41 @@ public class pickup : MonoBehaviour {
 
         if (other.gameObject.CompareTag("Damage"))
         {
-			if (lastDamageFrame + 300 <= Time.frameCount)
-			{
-				lastDamageFrame = Time.frameCount;
-				if (armor == 0)
-				{
-					health -= 5;
-					if (health <= 0)
-					{
-						print("Player is dead");
-						//player dies.
-					}
-				}
-				else
-				{
-					armor -= 5;
-					if (armor < 0)
-					{
-						armor = 0;
-					}
-				}
-				//other.gameObject.SetActive(false);
-			}
-		}
-    }
-
-    private void OnDamage(Collider other)
-    {
-        
+            int damVal = 0;
+            switch (objectName)
+            {
+                case "Enemy": //Damage taken on collision with enemy
+                    damVal = 10;
+                    break;
+                case "Bullet":  //Damage taken on collision with enemy projectile
+                    damVal = 5;
+                    break;
+                case "Environment": //Damage taken on collision with hazardous environment
+                    damVal = 7;
+                    break;
+            }
+            if (lastDamageFrame + 300 <= Time.frameCount)
+            {
+                lastDamageFrame = Time.frameCount;
+                if (armor == 0)
+                {
+                    health -= damVal;
+                    if (health <= 0)
+                    {
+                        print("Player is dead");
+                        //player dies.
+                    }
+                }
+                else
+                {
+                    armor -= damVal;
+                    if (armor < 0)
+                    {
+                        armor = 0;
+                    }
+                }
+                other.gameObject.SetActive(false);
+            }
+        }
     }
 }
