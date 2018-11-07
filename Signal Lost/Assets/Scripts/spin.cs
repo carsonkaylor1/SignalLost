b86718d4fedec7360efead35c220bb501b5838ed
carsonkaylor1 <ckaylor@email.sc.edu>
2018-11-06 20:11:37 -0500

    Add rover, wheel spin, rock pickup

---
+++ Signal Lost/Assets/Scripts/spin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour {
+
+    public int spinx = 0;
+    public int spiny = 0;
+    public int spinz = 0;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("Horizontal"))
+        {
+            Spin();
+        }
+
+
+        
+;	}
+
+    void Spin()
+    {
+        transform.Rotate(spinx, spiny, spinz);
+    }
+}
