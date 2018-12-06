076ac47d253f596106442015edc2697873a6717c
carsonkaylor1 <ckaylor@email.sc.edu>
2018-12-06 17:46:04 -0500

    smooth scene switch

---
+++ Signal Lost/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour {
+
+   // public 
+
+   // int playerCount = GameObject.FindGameObjectsWithTag("Player");
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        if(GameObject.FindGameObjectsWithTag("Player").Length > 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
