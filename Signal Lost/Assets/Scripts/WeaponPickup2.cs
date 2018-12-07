2ffa62217bfcf092b504b5b45f7a713c6aa6a561
carsonkaylor1 <ckaylor@email.sc.edu>
2018-12-07 02:13:38 -0500

    add stuff

---
+++ Signal Lost/Assets/Scripts/WeaponPickup2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickup2 : MonoBehaviour {
+
+
+    public GameObject myWeapon;
+    public GameObject weaponOnGround;
+    public Transform player;
+    public GameObject gobject;
+    // Use this for initialization
+    bool t = false;
+    private void OnTriggerEnter(Collider _collider)
+    {
+        if (_collider.gameObject.tag == "Player")
+        {
+            t = true;
+            gobject.transform.position = player.position;
+            //myWeapon.SetActive(true);
+           // weaponOnGround.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        if (t == true)
+        {
+            gobject.transform.position = player.position;
+        }
+	}
+}
