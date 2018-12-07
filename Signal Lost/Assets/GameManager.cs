d83682d504769b3ef3182e499c5a0d107a5b5851
TristanK <frostflag@gmail.com>
2018-12-07 02:55:00 -0500

    add game manager to handle end game

---
+++ Signal Lost/Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	bool gameHasEnded = false;
+
+	public void EndGame()
+	{
+		if(gameHasEnded == false)
+		{
+			gameHasEnded = true;
+			Debug.Log("Game Over");
+			Restart();
+		}
+	}
+
+	void Restart()
+	{
+		SceneManager.LoadScene(0);
+	}
+	
+}
---
+++ Signal Lost/Assets/Scripts/winGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winGame : MonoBehaviour {
+
+	public GameObject circuit;
+	public GameManager gameManager;
+
+	bool bossIsDead;
+
+	// Use this for initialization
+	void Start () {
+
+		if(circuit == null)
+		{
+			circuit = GameObject.FindWithTag("Boss");
+			
+		}
+		if(circuit.tag == "Boss")
+		{
+			Debug.Log("Found boss");
+			bossIsDead = false;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(circuit == null)
+		{
+			bossIsDead = true;
+		}
+		if(bossIsDead)
+		{
+			FindObjectOfType<GameManager>().EndGame();
+
+		}
+	}
+}
