bef39d11e41830fb5bc165699727e8616b389f88
TristanK <frostflag@gmail.com>
2018-12-07 01:27:21 -0500

    this stuff came from building

---
+++ Signal Lost/Assets/winGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class winGame : MonoBehaviour {
+
+	public GameObject circuit;
+	// Use this for initialization
+	void Start () {
+		circuit = GameObject.GetComponent<EnemyHealth>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if()
+	}
+}
